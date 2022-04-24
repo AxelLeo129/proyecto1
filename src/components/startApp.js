5d9d03033f8acc961d767c2e5a536aa7e0099de5
AxelLeo129 <axelleo129dev@gmail.com>
2022-04-23 23:45:48 -0600

    Project structure.

---
+++ src/components/startApp.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./startApp.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./startApp.css";
+
+class StartApp extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="global">
+          <div className="card card--width">
+            <div className="card-header">
+              <h5>Proyecto 1</h5>
+              <h6>Memoria Sistemas y Tecnologías Web</h6>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Axel Leonardo López Barrera 20768</h5>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StartApp;
